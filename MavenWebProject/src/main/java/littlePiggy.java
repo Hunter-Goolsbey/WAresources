b3f3e8edc89c1898cd0738de885fb019c5e268cd
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-18 10:07:10 -0700

    Changed piggy servlet config settings urlPatterns

--- MavenWebProject/src/main/java/littlePiggy.java
+++ MavenWebProject/src/main/java/littlePiggy.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class littlePiggy
  */
-@WebServlet("/littlePiggy")
+@WebServlet(urlPatterns = {"/littlePiggy"})
 public class littlePiggy extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
