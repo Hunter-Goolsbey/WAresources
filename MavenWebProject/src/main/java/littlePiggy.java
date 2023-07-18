1c4657a1df33d7c08353b41d0abe7b30084106a9
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-17 20:32:29 -0700

    added piggy slash

--- MavenWebProject/src/main/java/littlePiggy.java
+++ MavenWebProject/src/main/java/littlePiggy.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class littlePiggy
  */
-@WebServlet("/littlePiggy")
+@WebServlet("/littlePiggy/")
 public class littlePiggy extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
