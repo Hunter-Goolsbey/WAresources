e4885efcae64c27b45ea16ae1ab272474fd798bc
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-17 20:44:40 -0700

    updated servlet settings

--- MavenWebProject/src/main/java/littlePiggy.java
+++ MavenWebProject/src/main/java/littlePiggy.java
@@ -42,7 +42,6 @@ public class littlePiggy extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
     @Override
-    //
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
     	response.setContentType("text/html");
