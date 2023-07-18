79f823c5458c01a25a6aabc1f477964ab7c3da35
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-18 11:20:44 -0700

    added index.jsp

--- MavenWebProject/src/main/java/littlePiggy.java
+++ MavenWebProject/src/main/java/littlePiggy.java
@@ -22,26 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 public class littlePiggy extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public littlePiggy() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-    @Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
     @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
---
+++ MavenWebProject/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<body>
+		<h2>Maven JSP and Servlet Hello World!</h2>
+		<form action="littlePiggy" method="post">
+			Enter Name: <input type="text" name="name" size="20"/>
+			<input type="submit" value="Go to Market" />
+		</form>
+	</body>
+</html>
