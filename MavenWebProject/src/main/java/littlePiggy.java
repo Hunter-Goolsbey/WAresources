bf9f3a97474100bfa1e818ed5f3cc82a3bd11424
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-19 14:31:41 -0700

    update config and directory structure

--- MavenWebProject/src/main/java/littlePiggy.java
+++ MavenWebProject/src/main/java/littlePiggy.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,44 +11,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
-
 /**
  * Servlet implementation class littlePiggy
  */
-
-
 
 @WebServlet("/littlePiggy")
 public class littlePiggy extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    
-    @Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-    	response.setContentType("text/html");
+		response.setContentType("text/html");
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String pw = System.getenv("SQLJAVA");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/HelloWorld","ducky",pw);
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/HelloWorld", "ducky", pw);
 			Statement stm = con.createStatement();
-			ResultSet rs = stm.executeQuery("select * from UserCredentials where Username='" + username + "'and Password='" + password +"';");
-			if(rs.next()) {
+			ResultSet rs = stm.executeQuery(
+					"select * from UserCredentials where Username='" + username + "'and Password='" + password + "';");
+			if (rs.next()) {
 				response.sendRedirect("Home.html");
-				//System.out.println(Integer.toString(2+2));
+				// System.out.println(Integer.toString(2+2));
 			} else {
 				response.getWriter().println("wrong username and password");
 			}
 			con.close();
-		}catch (Exception e){
-			//response.sendRedirect("Home.html");
+		} catch (Exception e) {
+			// response.sendRedirect("Home.html");
 			System.out.println("Connection not Made");
 			System.out.println(e);
 		}
-    }
+	}
 }
