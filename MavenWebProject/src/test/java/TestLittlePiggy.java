0b045e7c6c06c4dd3119a0ce90387a739611bf66
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-20 10:20:18 -0700

    updated pom, corrected/cleaned webdriver configuration

--- MavenWebProject/src/test/java/TestLittlePiggy.java
+++ MavenWebProject/src/test/java/TestLittlePiggy.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 //import static org.testng.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class TestLittlePiggy {
 	
@@ -20,18 +21,27 @@ public class TestLittlePiggy {
 		String PW = System.getenv("TEST_PASS_BASIC");
 		System.setProperty("webdriver.chrome.driver", "/Users/The-Intern/Downloads/chromedriver");
 		ChromeOptions options = new ChromeOptions();
+		//options.addArguments("-headless");
 		options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
 		
 		
 		WebDriver driver = new ChromeDriver(options);
+		//HtmlUnitDriver unitDriver = new HtmlUnitDriver(true);
 		driver.manage().window().maximize();
+		System.out.println("Started Selenium");
 		driver.get("http://34.193.84.77:8080/MavenWebProject/");
+		System.out.println("Entered webpage");
+		Thread.sleep(3000);
+		System.out.println((String)driver.getCurrentUrl());
 		driver.findElement(By.xpath("/html/body//form[@action='littlePiggy']/table//input[@name='username']")).sendKeys(UN);
 		driver.findElement(By.xpath("/html/body//form[@action='littlePiggy']/table//input[@name='password']")).sendKeys(PW);
 		driver.findElement(By.xpath("/html/body//form[@action='littlePiggy']/table//input[@value='login']")).click();
+		System.out.println("Passed Login");
 		Thread.sleep(3000);
 		assertEquals((String)driver.getCurrentUrl(), "http://34.193.84.77:8080/MavenWebProject/Home.html");
-		driver.close();
+		System.out.println((String)driver.getCurrentUrl());
+		driver.quit();
+		//driver.close();
 	}
 	
 	public void main (String[] args) throws InterruptedException {
