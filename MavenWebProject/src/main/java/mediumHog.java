6ea888cf8e4584aef1452892205164170bda2798
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-07-30 20:04:32 -0700

    Test integration of mediumHog and Home modal

--- MavenWebProject/src/main/java/mediumHog.java
+++ MavenWebProject/src/main/java/mediumHog.java
@@ -13,20 +13,13 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class mediumHog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public mediumHog() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+    	
+    	response.setContentType("text/html");
+    	response.getWriter().println("<h1>Wrong username and password</h1>");
+    	
 	}
 
 }
