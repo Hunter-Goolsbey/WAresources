95478e0be700634da6123d9446e27872394bbfde
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-08-02 09:17:49 -0700

    Updated comments. Cleaned up unused code

--- MavenWebProject/src/test/java/TestHome.java
+++ MavenWebProject/src/test/java/TestHome.java
@@ -12,13 +12,16 @@ import org.openqa.selenium.By;
 
 
 public class TestHome {
+	//*Sets up driver instance and saves public variables*
+	
 	//SeleniumConfig SelenSetup = new SeleniumConfig();
 	WebDriver driver = SeleniumConfig.driverSetup();
 	String home = "http://34.193.84.77:8080/MavenWebProject/Home.html";
 	
-	//Functionality has been removed from Home page
+	//RM: Function no longer included
 //  @Test
 //  public void test_Home_isCollapsed() throws InterruptedException {
+	//*Test to see if collapsable paine is-collapsed on-load*
 //	  WebDriver driver = SelenSetup.driverSetup();
 //	  driver.get("http://34.193.84.77:8080/MavenWebProject/Home.html");
 //	  driver.findElement(By.xpath("/html//div[@id='accordion']//a[@href='#collapseOne']")).click();
@@ -30,6 +33,7 @@ public class TestHome {
   
   @Test
   public void Test_NavMenu_isHidden() throws InterruptedException {
+	  //*Tests to ensure modal navigation panel is closed on-load*
 	  driver.get(home);
 	  Thread.sleep(3000); //Needed
 	  //driver.findElement(By.cssSelector("[data-bs-toggle='offcanvas']")).click();
@@ -46,15 +50,14 @@ public class TestHome {
 	  driver.findElement(By.linkText("MyGarage")).click();
 	  Thread.sleep(1000);
 	  assertEquals(driver.getCurrentUrl(), home);
-	  //driver.quit();
   }
+  
   @Test
   public void searchGarage_Does_Reduce() throws InterruptedException {
 	  int i = 0;
 	  List<WebElement> searchResults;
 	  int count = 0;
 	  
-	  //RM: WebDriver driver = SelenSetup.driverSetup();
 	  driver.get(home);
 	  Thread.sleep(1000);
 	  
@@ -70,21 +73,19 @@ public class TestHome {
 		  }
 	  }
 	  assertEquals(2, count); //2 swap out with direct SQL COUNT query once tables are created
-	  //RM: driver.quit();
   }
   
   @Test
   public void addVehicle_verifyInput() throws InterruptedException{
-	  //WebDriver driver = SelenSetup.driverSetup();
 	  driver.get(home);
 	  Thread.sleep(1000);
 	  driver.findElement(By.cssSelector("[data-bs-toggle='modal']")).click();
 	  driver.findElement(By.cssSelector("[placeholder='VIN']")).sendKeys("5UXWX7C5*BA");
 	  driver.findElement(By.cssSelector("input[name='remember']")).click();
 	  Thread.sleep(3000);
-	  //System.out.println(driver.findElement(By.cssSelector("#vehicleMake-info")).getText());
 	  assertEquals("BMW",driver.findElement(By.cssSelector("#vehicleMake-info")).getText());
   }
+  
   @AfterTest
   public void cleanup(){
   driver.quit();
