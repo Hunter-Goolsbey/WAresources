4319562cf6aa13caa847c75c2da0b02c747f3070
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-08-04 14:44:21 -0700

    cleanup

--- MavenWebProject/src/main/java/mediumHog.java
+++ MavenWebProject/src/main/java/mediumHog.java
@@ -3,11 +3,11 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
