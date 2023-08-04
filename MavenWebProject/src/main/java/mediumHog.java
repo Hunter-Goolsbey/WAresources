16072baf7877c7bfb52c6c838945509a139e136a
Hunter-Goolsbey <huntergoolsbey@gmail.com>
2023-08-04 11:23:04 -0700

    log vehicle id in error report

--- MavenWebProject/src/main/java/mediumHog.java
+++ MavenWebProject/src/main/java/mediumHog.java
@@ -51,6 +51,7 @@ public class mediumHog extends HttpServlet {
 			// response.sendRedirect("Home.jsp");
 			//System.out.println("Connection not Made");
 			response.getWriter().println(e);
+			response.getWriter().println(id);
 			//response.getWriter().println(pw);
 			//System.out.println(e);
 		}
